e31f69ee2f0b5946968b5847f47d92217810cace
Bertina Ayuure <bertina.ayuure@amalitech.com>
2023-01-16 08:33:25 +0000

    Create childTab.cy.js
    handling child tabs using cypress

---
+++ cypress/e2e/childTab.cy.js
@@ -0,0 +1,30 @@
+describe('Child Tabs',()=>{
+
+    it('Approach1', () => {
+
+        cy
+        .visit('https://the-internet.herokuapp.com/windows')
+        cy
+        .get('.example>a')
+        .invoke('removeAttr','target')
+        .click()
+        cy.url().should('include','https://the-internet.herokuapp.com/windows/new')
+        cy
+        .go('back')
+        
+    });
+
+    it('Approach2', () => {
+        cy
+        .visit('https://the-internet.herokuapp.com/windows')
+
+        cy.get('.example>a').then((e)=>{
+            let url = e.prop('href')
+
+            cy.visit(url)
+            cy.url().should('include','https://the-internet.herokuapp.com/windows/new')
+            cy.go('back')
+        })
+        
+    });
+})
