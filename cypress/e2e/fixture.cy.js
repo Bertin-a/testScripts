6b3f2e9569cc21f8ee334f4a28c1680e9ca6169e
Bertina Ayuure <bertina.ayuure@amalitech.com>
2023-01-18 09:31:15 +0000

    bugs in fixtures fixed

--- cypress/e2e/fixture.cy.js
+++ cypress/e2e/fixture.cy.js
@@ -1,47 +1,90 @@
 
 
-describe('Fixtures demo',()=>{
-let userData
+// describe('Fixtures demo',()=>{
+//     let userData;
 
+<<<<<<< Updated upstream
 beforeEach('Login',()=>{
     cy.fixture().then((e)=>{
         userData=e
     })
+=======
+// before('Login',()=>{
+//     cy.fixture('orangehrm').then((data)=>{
+//         userData=data
+//     })
+>>>>>>> Stashed changes
     
-})
-//Direct Access
-    it.skip('FixturesDemoTest', () => {
+// })
+// //Direct Access
+//     it('FixturesDemoTest', () => {
 
-        cy
-        .visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+//         cy
+//         .visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
 
-        cy
-        .get('input[placeholder="Username"]')
-        .type('Admin')
+//         cy
+//         .get('input[placeholder="Username"]')
+//         .type('Admin')
 
-        cy
-        .get('input[placeholder="Password"]')
-        .type('admin123')
+//         cy
+//         .get('input[placeholder="Password"]')
+//         .type('admin123')
 
-        cy
-        .get('button[type="submit"]')
-        .click()
+//         cy
+//         .get('button[type="submit"]')
+//         .click()
 
-        cy
-        .get('span[class="oxd-topbar-header-breadcrumb"]')
-        .should('be.visible').and('have.text','Dashboard')
-
-
-    });
+//         cy
+//         .get('span[class="oxd-topbar-header-breadcrumb"]')
+//         .should('be.visible').and('have.text','Dashboard')
 
 
+//     });
+
+
+//     })
+    
+
+//     it.only('FixturesDemoTest', () => {
+
+//         cy
+//         .visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+        
+//         cy
+//         .get('input[placeholder="Username"]')
+//         .type(userData.username)
+
+//         cy
+//         .get('input[placeholder="Password"]')
+//         .type(userData.password)
+
+//         cy
+//         .get('button[type="submit"]')
+//         .click()
+
+//         cy
+//         .get('span[class="oxd-topbar-header-breadcrumb"]')
+//         .should('be.visible')
+//         .and('have.text', userData.expected)
+
+        
+//      })
+
+describe('',()=>{
+
+    let userData
+
+    before('',()=>{
+        cy.fixture('orangehrm').then((e)=>{
+            userData=e
+        })
     })
 
-    it('FixturesDemoTest', () => {
+    it('', () => {
 
         cy
         .visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-        
+
         cy
         .get('input[placeholder="Username"]')
         .type(userData.username)
@@ -51,15 +94,21 @@ beforeEach('Login',()=>{
         .type(userData.password)
 
         cy
-         .get('button[type="submit"]')
+        .get('button[type="submit"]')
         .click()
 
         cy
         .get('span[class="oxd-topbar-header-breadcrumb"]')
+<<<<<<< Updated upstream
         .should('be.visible').and('have.text', userData.expected)
+=======
+        .should('be.visible')
+        .and('have.text','Dashboard')
+>>>>>>> Stashed changes
 
         
-     })
+    });
+})
 
        
 
