ddff4f02c67a907d7ae2ae1793fcab90e49ef42f
Bertina Ayuure <bertina.ayuure@amalitech.com>
2023-01-27 13:40:37 +0000

    Update alis&invoke.cy.js

--- cypress/e2e/alis&invoke.cy.js
+++ cypress/e2e/alis&invoke.cy.js
@@ -42,11 +42,123 @@ describe('',()=>{
         .find('.productcart')
         .invoke('attr','title')
         .should('include','Add to Cart')
+})
+
+it('normal product', () => {
+
+    cy
+    .visit('https://automationteststore.com/')
+
+    cy
+    .get('.thumbnail')
+    .as('productThumbnail')
+
+    cy
+    .get('@productThumbnail')
+    .find('.oneprice')
+    .each(($el, index, $list)=>{
+        let price=$el.text()
+        cy.log(price)
+    })
+
+    
+});
+
+it('price of normal sale items', () => {
+
+    cy
+    .visit('https://automationteststore.com/')
+
+    cy
+    .get('.thumbnail')
+    .find('.oneprice')
+    .invoke('text')
+    .as('itemPrice')
+
+    cy
+    .get('@itemPrice')
+    .then($linkText=>{
+        let totalPrice =0
+        let itemsPrice = $linkText.split('$')
+        
+         for(let i=0; i<itemsPrice.length; i++){
+             cy.log(itemsPrice[i])
+             totalPrice+=Number(itemsPrice[i])
+        }
+        
+        cy.log('Normal sale items'+totalPrice)
+     })
+
+    
+});
+
+it.only('calculating the total price', () => {
+
+    cy
+    .visit('https://automationteststore.com/')
+
+    cy
+    .get('.thumbnail')
+    .find('.oneprice')
+    .invoke('text')
+    .as('itemPrice')
+
+    let totalPriceOfNormalItems=0
+   
+    let total=0
+
+    cy
+    .get('@itemPrice')
+    .then($linkText=>{
+        
+        let normalPriceItems = $linkText.split('$')
+
+        for(let i=0; i<normalPriceItems.length; i++)
+        {
+             total+=Number(normalPriceItems[i])
+             cy.log(total)
+            
+        }
+         totalPriceOfNormalItems+=total
+    
+
+         //cy.log(totalPriceOfNormalItems)
+
+    })
+
+    cy
+    .get('.thumbnail')
+    .find('.pricenew')
+    .invoke('text')
+    .as('salePrice')
+
+    cy
+    .get('@salePrice')
+    .then($linkText=>{
+        total
+        let normalPriceItems = $linkText.split('$')
+
+        for(let i=0; i<normalPriceItems.length; i++)
+        {
+             total+=Number(normalPriceItems[i])
+
+             //cy.log("The total price of "+total)
+            
+        }
+         totalPriceOfNormalItems+=total
+
+         cy.log(totalPriceOfNormalItems)
+
+    })
+
+    
 
 
     
-       
+});
 
-})
+
+
+    
 })
 
